8fc9bbe8ec9ef3c1d67fda9334eaed20d9651583
calamoazul <info@calamoazul.com>
2021-09-30 01:39:02 +0200

    cambios 30 septiembre

--- js/cookies.js
+++ js/cookies.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
    var cookies = $('#alertcookies');
-   var aceptarcookies = $('#aceptar-cookies');
-   var rechazarcookies = $('#rechazar-cookies');
-   function aprobarcookies() {
-     alert('Se han aprobado las cookies');}
-   function suspendercookies () {
-       alert('Se han eliminado las cookies');}
-   if (aceptarcookies.click(aprobarcookies()) || rechazarcookies.click(suspendercookies())){
-    cookies.hide();}
-   else {
-    alert('algo ha salido mal');
-  }
+   var aceptar = $('#aceptarCookies');
+   var rechazar = $('#rechazarCookies');
+   function aprobarcookies(){
+      aceptarcookies.on('click', function(){
+        alert('Se han aprobado las cookies');
+        cookies.hide();
+      })}
+   function suspendercookies(){
+      rechazarcookies.on('click', function(){
+        alert('Se han rechazado cookies');
+        cookies.hide();
+      })}
 });
